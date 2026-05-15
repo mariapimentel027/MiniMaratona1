097fda09154996c837a695da1aecbff7e87c0eb3
mariapimentel.pf027 <mariapimentel.pf027@academico.ifsul.edu.br>
2026-05-15 15:01:20 -0300

    inicializando a função

---
+++ q2_MariaeVic.js/q2.js
@@ -0,0 +1,14 @@
+function solucao(texto){
+    let textominusculo = texto.toLowerCase();
+    //ignorando letras minusculas
+    let textoinvertido = "";
+  for (let i = textoMinusculo.length - 1; i >= 0; i--) {
+        textoInvertido += textoMinusculo[i];
+    }
+    // invertendo a string usando um laço => pesquisei tbm  
+
+    return textominusculo === textoinvertido;
+    // comparando os textos
+
+    
+}
